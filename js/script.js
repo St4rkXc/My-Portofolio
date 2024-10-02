6cefeb53b61b594df56407736c7f4bd733822586
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-10-02 19:41:56 +0800

    Intro & Hero section

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// function userScroll() {
+//   const navbar = document.querySelector(".navbar");
+//   window.addEventListener("scroll", () => {
+//     if (window.scrollY > 50) {
+//       navbar.classList.add("navbar-dark");
+//       navbar.classList.add("text-bg-primary");
+//     } else {
+//       navbar.classList.remove("navbar-dark");
+//       navbar.classList.remove("text-bg-primary");
+//     }
+//   });
+// }
+
+// document.addEventListener("DOMContentLoaded", userScroll);
+
+
