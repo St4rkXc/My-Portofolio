2ee8aa9bd80e4fb9ba60970cd25f6df3a926bafc
Stark <123723458+St4rkXc@users.noreply.github.com>
2024-10-06 14:14:54 +0800

    Jjavascript function for contact

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 AOS.init();
+
+const myform = document.querySelector(".contact-form");
+const nameform = document.querySelector(".form-name");
+const email = document.querySelector(".form-email");
+const message = document.querySelector(".form-message");
+const submitButton = document.querySelector(".subbtn");
+
+console.log(myform);
+console.log(nameform);
+console.log(email);
+console.log(message);
+
+myform.addEventListener("submit", (e) => {
+   e.preventDefault();
+   console.log("submit");
+   if (nameform.value == "" || email.value == "" || message.value == "") {
+   } else {
+      submitButton.innerText = "Thank You For Submitting!";
+      submitButton.id = 'secondbut'
+      nameform.value = ""
+      email.value = ""
+      message.value = ""
+      
+   }
+});
